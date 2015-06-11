df1f4a98d749a695a3a5e40d35542c869d891fe8
Deepu Mohan Puthrote <github@deepumohan.com>
2015-06-11 11:37:40 +0100

    Migrate main method to use checkout with offers

--- src/main/scala/com/github/warfox/shoppingcart/Main.scala
+++ src/main/scala/com/github/warfox/shoppingcart/Main.scala
@@ -3,7 +3,7 @@ package com.github.warfox.shoppingcart
 object Main {
 
   def main(args: Array[String]) {
-    println("[ " + args.mkString(", ") + " ] => £" + ShoppingCart.checkout(args))
+    println("[ " + args.mkString(", ") + " ] => £" + ShoppingCart.checkoutWithOffers(args))
   }
 
 }
