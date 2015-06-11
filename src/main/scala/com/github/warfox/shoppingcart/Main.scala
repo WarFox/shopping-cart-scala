95190b325b303c87e5732d39c30c07655f0a9e23
Deepu Mohan Puthrote <github@deepumohan.com>
2015-06-11 08:19:50 +0100

    Added main method that accepts array

---
+++ src/main/scala/com/github/warfox/shoppingcart/Main.scala
@@ -0,0 +1,9 @@
+package com.github.warfox.shoppingcart
+
+object Main {
+
+  def main(args: Array[String]) {
+    println("[ " + args.mkString(", ") + " ] => £" + ShoppingCart.checkout(args))
+  }
+
+}
