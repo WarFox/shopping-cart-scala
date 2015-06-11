a9dbe303dd5007af993e953cb2038ad61fc9cc07
Deepu Mohan Puthrote <github@deepumohan.com>
2015-06-11 08:01:22 +0100

    Extracted getPrice method

--- src/main/scala/com/github/warfox/shoppingcart/ShoppingCart.scala
+++ src/main/scala/com/github/warfox/shoppingcart/ShoppingCart.scala
@@ -4,15 +4,19 @@ import scala.math.BigDecimal.RoundingMode
 
 object ShoppingCart {
 
-  val priceList: Map[String, Double] = Map(("apple", 0.60), ("orange", 0.25))
+  private val priceList: Map[String, Double] = Map(("apple", 0.60), ("orange", 0.25))
 
   def checkout(items: Array[String]): Double = {
     var total: Double = 0.0
     for (item <- items) {
-      val price: Double = priceList.get(item toLowerCase()).get
+      val price: Double = getPrice(item)
       total += price
     }
     BigDecimal(total).setScale(2, RoundingMode.HALF_EVEN).toDouble
+  }
+
+  def getPrice(item: String): Double = {
+    priceList.getOrElse(item toLowerCase(), 0)
   }
 
 }
--- src/test/scala/com/github/warfox/shoppingcart/ShoppingCart$Test.scala
+++ src/test/scala/com/github/warfox/shoppingcart/ShoppingCart$Test.scala
@@ -5,11 +5,15 @@ import org.specs2.mutable.Specification
 class ShoppingCart$Test extends Specification {
 
   "Price of an apple is 0.60'" in {
-    ShoppingCart.priceList.get("apple").get must be equalTo 0.60
+    ShoppingCart.getPrice("apple") must be equalTo 0.60
   }
 
   "Price of an orange is 0.25'" in {
-    ShoppingCart.priceList.get("orange").get must be equalTo 0.25
+    ShoppingCart.getPrice("orange") must be equalTo 0.25
+  }
+
+  "Price of item that doesn't exist is 0" in {
+    ShoppingCart.getPrice("nonexistent") must be equalTo 0
   }
 
   "2 Apples should cost 1.20 on checkout" in {
