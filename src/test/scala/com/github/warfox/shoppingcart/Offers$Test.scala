714faa62ed49d7f08b09d01c2c4c1e54273acbe5
Deepu Mohan Puthrote <github@deepumohan.com>
2015-06-11 10:46:46 +0100

    Added more tests for offers

--- src/test/scala/com/github/warfox/shoppingcart/Offers$Test.scala
+++ src/test/scala/com/github/warfox/shoppingcart/Offers$Test.scala
@@ -12,12 +12,20 @@ class Offers$Test extends Specification {
     Offers.buyOneGetOne(3 , 0.50) must be equalTo 1.00
   }
 
+  "3 items should cost 0.50 when price for 1 is 0.25, on buyOneGetOne offer" in {
+    Offers.buyOneGetOne(3 , 0.25) must be equalTo 0.50
+  }
+
   "4 items should cost 1.00 when price for 1 is 0.50, on buyOneGetOne offer" in {
     Offers.buyOneGetOne(3 , 0.50) must be equalTo 1.00
   }
 
   "3 items should cost 1.00 when price for 1 is 0.50, on threeForTwo offer" in {
     Offers.threeForTwo(3, 0.50) must be equalTo 1.00
+  }
+
+  "7 items should cost 1.00 when price for 1 is 0.50, on threeForTwo offer" in {
+    Offers.threeForTwo(7, 0.25) must be equalTo 1.25
   }
 
 }
