33acce6ebaed14c1eb936acd976416aa30262d0f
Deepu Mohan Puthrote <github@deepumohan.com>
2015-06-11 07:25:33 +0100

    Corrected checkout method to get right price for 2 apples

--- src/main/scala/com/github/warfox/shoppingcart/ShoppingCart.scala
+++ src/main/scala/com/github/warfox/shoppingcart/ShoppingCart.scala
@@ -5,7 +5,12 @@ object ShoppingCart {
   val priceList: Map[String, Double] = Map(("apple", 0.60), ("orange", 0.25))
 
   def checkout(items: Array[String]): Double = {
-    0
+    var total: Double = 0.0
+    for (item <- items) {
+      val price: Double = priceList.get(item).get
+      total += price
+    }
+    total
   }
 
 }
