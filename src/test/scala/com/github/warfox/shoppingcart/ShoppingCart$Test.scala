4deed3c2c73227dbd98deb0d4ec02f6382fe4909
Deepu Mohan Puthrote <github@deepumohan.com>
2015-06-11 07:37:21 +0100

    Added more tests

--- src/test/scala/com/github/warfox/shoppingcart/ShoppingCart$Test.scala
+++ src/test/scala/com/github/warfox/shoppingcart/ShoppingCart$Test.scala
@@ -16,12 +16,24 @@ class ShoppingCart$Test extends Specification {
     ShoppingCart.checkout(Array("apple", "apple")) must be equalTo 1.20
   }
 
+  "3 Apples should cost 1.80 on checkout" in {
+    ShoppingCart.checkout(Array("apple", "apple", "apple")) must be equalTo 1.8
+  }
+
+  "4 Apples should cost 2.40 on checkout" in {
+    ShoppingCart.checkout(Array("apple", "apple", "apple", "apple")) must be equalTo 2.40
+  }
+
   "2 Oranges should cost 0.5 on checkout" in {
     ShoppingCart.checkout(Array("orange", "orange")) must be equalTo 0.5
   }
 
-  "3 Apples should cost 1.80 on checkout" in {
-    ShoppingCart.checkout(Array("apple", "apple", "apple")) must be equalTo 1.8
+  "3 Oranges should cost 0.75 on checkout" in {
+    ShoppingCart.checkout(Array("orange", "orange", "orange")) must be equalTo 0.75
+  }
+
+  "4 Oranges should cost 1 on checkout" in {
+    ShoppingCart.checkout(Array("orange", "orange", "orange", "orange")) must be equalTo 1
   }
 
 }
