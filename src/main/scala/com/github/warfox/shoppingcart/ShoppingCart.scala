1171aa454a22164b60b467ca7b01811d7a8e76be
Deepu Mohan Puthrote <github@deepumohan.com>
2015-07-07 21:22:52 +0100

    Simplified checkout function using flatmap

--- src/main/scala/com/github/warfox/shoppingcart/ShoppingCart.scala
+++ src/main/scala/com/github/warfox/shoppingcart/ShoppingCart.scala
@@ -7,26 +7,23 @@ object ShoppingCart {
   private val priceList: Map[String, Double] = Map(("apple", 0.60), ("orange", 0.25), ("banana", 0.20))
 
   def checkout(items: Array[String]): Double = {
-    var total: Double = 0.0
-    for (item <- items) {
-      val price: Double = getPrice(item)
-      total += price
-    }
+    val total = items.flatMap(price).sum
     BigDecimal(total).setScale(2, RoundingMode.HALF_EVEN).toDouble
   }
 
   def checkoutWithOffers(items: Array[String]): Double = {
-    var total: Double = 0.0
     val noOfApples: Int = items.count(p => p.equalsIgnoreCase("apple"))
     val noOfOranges: Int = items.count(p => p.equalsIgnoreCase("orange"))
     val noOfBananas: Int = items.count(p => p.equalsIgnoreCase("banana"))
 
-    total = Offers.offer("apple")(noOfApples) + Offers.offer("orange")(noOfOranges) + Offers.offer("banana")(noOfBananas)
+    val total = Offers.offer("apple")(noOfApples) + Offers.offer("orange")(noOfOranges) + Offers.offer("banana")(noOfBananas)
     BigDecimal(total).setScale(2, RoundingMode.HALF_EVEN).toDouble
   }
 
   def getPrice(item: String): Double = {
     priceList.getOrElse(item toLowerCase(), 0)
   }
+
+  def price(item: String): Option[Double] = priceList.get(item toLowerCase)
 
 }
